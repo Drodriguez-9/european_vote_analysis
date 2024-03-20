5c27098f736bcc661de5c103d41998422ce3da8e
Diego Rodriguez <s222883@student.dtu.dk>
2024-03-20 16:28:39 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='European countries national political data (votes, EU-closeness,..) and European Elections data -> goal to predict the outcome of EU elections 2024',
+    author='group_19',
+    license='',
+)

5543e3b11fca52fd899ec654b413590e9e2e71e2
Miguel Bustorff <mbustorff@gmail.com>
2024-05-28 13:14:26 +0200

    IV analysis code

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 def calculate_columns(df: pd.DataFrame):
     # Calculate the total amount of interest
-    df['TotalInterest'] = df['LoanAmount'] * df['InterestRate'] * df['LoanTerm']
+    df['_TotalInterest'] = df['LoanAmount'] * df['InterestRate'] * df['LoanTerm']
     # Other derived columns
-    df['MonthlyPayment'] = df['LoanAmount'] / df['LoanTerm']
-    df['TotalPayment'] = df['LoanAmount'] + df['TotalInterest']
-    df['LoanToIncomeRatio'] = df['LoanAmount'] / df['Income']
-    df['RemainingIncomeAfterPayment'] = df['Income'] - df['MonthlyPayment']
-    df['YearsEmployed'] = df['MonthsEmployed'] / 12
-    df['LoanTermYears'] = df['LoanTerm'] / 12
-    df['InterestToPrincipalRatio'] = df['TotalInterest'] / df['LoanAmount']
+    df['_MonthlyPayment'] = df['LoanAmount'] / df['LoanTerm']
+    df['_TotalPayment'] = df['LoanAmount'] + df['_TotalInterest']
+    df['_LoanToIncomeRatio'] = df['LoanAmount'] / df['Income']
+    df['_RemainingIncomeAfterPayment'] = df['Income'] - df['_MonthlyPayment']
+    df['_YearsEmployed'] = df['MonthsEmployed'] / 12
+    df['_LoanTermYears'] = df['LoanTerm'] / 12
+    df['_InterestToPrincipalRatio'] = df['_TotalInterest'] / df['LoanAmount']
+    df["_NetIncome"]=df["Income"]*(1-df["DTIRatio"])
     return df

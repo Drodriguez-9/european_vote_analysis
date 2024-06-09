927c67e8abe26f3cb744a08b197571301085efdb
Poul Gunnar Pii Svane <s153529@student.dtu.dk>
2024-06-09 13:08:37 +0200

    work on classifier based weights

--- src/models/PGMs.py
+++ src/models/PGMs.py
@@ -41,3 +41,99 @@ def model_hierarchical(X, n_cat1, n_cat2, cat2, obs=None):
     return y
 
 
+# first try
+def model_multi_hierarchical(X, categoricals, n_cat, obs=None):
+    input_dim = X.shape[1]
+    cat_dim = categoricals.shape[1]
+
+    logits = 0
+
+    #importance = pyro.sample("importance", dist.HalfCauchy(10. * torch.ones(cat_dim)).to_event())
+
+
+    for cat in  pyro.plate("cats", cat_dim):
+        # Prior for the bias mean
+        alpha_mu = pyro.sample("alpha_mu{cat}",
+                               dist.Normal(torch.zeros(2),
+                                           10. * torch.ones(2)).to_event())
+
+        # Prior for the bias standard deviation
+        alpha_sigma = pyro.sample("alpha_sigma{cat}",
+                                  dist.HalfCauchy(10. * torch.ones(2)).to_event())
+        # Prior for the weight mean
+        beta_mu = pyro.sample("beta_mu{cat}",
+                              dist.Normal(torch.zeros(2),
+                                          10. * torch.ones(2)).to_event())
+
+        # Prior for the weight standard deviation
+        beta_sigma = pyro.sample("beta_sigma{cat}",
+                                 dist.HalfCauchy(10. * torch.ones(2)).to_event())
+
+        with pyro.plate("ind", n_cat):
+            # Draw the individual parameter for each individual
+            alpha = pyro.sample("alpha_{cat}",
+                                dist.Normal(alpha_mu, alpha_sigma).to_event(1))
+
+            # Priors for the regression coefficents
+            beta = pyro.sample("beta_{cat}",
+                               dist.Normal(beta_mu, beta_sigma).to_event(1))
+
+        logits += alpha + X.matmul(beta)
+
+
+    with pyro.plate("data", X.shape[0]):
+        logits = alpha[ind] + X.matmul(beta)
+        # If you use logits you don't need to do sigmoid
+        y = pyro.sample("y",
+                        dist.Categorical(logits=logits), obs=obs)
+
+    return y
+
+
+# second try
+def model2_multi_hierarchical(X, categoricals, obs=None):
+    input_dim = X.shape[1]
+    cat_dim = categoricals.shape[1]
+
+    logits = 0
+
+    # Prior for the bias mean
+    alpha_mu = pyro.sample("alpha_mu",
+                           dist.Normal(torch.zeros(cat_dim, 2),
+                                       10. * torch.ones(cat_dim, 2)).to_event())
+
+    # Prior for the bias standard deviation
+    alpha_sigma = pyro.sample("alpha_sigma",
+                              dist.HalfCauchy(10. * torch.ones(cat_dim, 2)).to_event())
+    # Prior for the weight mean
+    beta_mu = pyro.sample("beta_mu",
+                          dist.Normal(torch.zeros(input_dim, cat_dim, 2),
+                                      10. * torch.ones(input_dim, cat_dim, 2)).to_event())
+
+    # Prior for the weight standard deviation
+    beta_sigma = pyro.sample("beta_sigma",
+                             dist.HalfCauchy(10. * torch.ones(input_dim, cat_dim, 2)).to_event())
+
+    l_prior = pyro.sample("l_prior", dist.Beta(2. * torch.ones(cat_dim),
+                                               3. * torch.ones(cat_dim)).to_event())
+
+    with pyro.plate("inputs", input_dim):
+        # Draw the individual parameter for each individual
+        alpha = pyro.sample("alpha",
+                            dist.Normal(alpha_mu, alpha_sigma).to_event(1))
+
+        # Priors for the regression coefficents
+        beta = pyro.sample("beta",
+                           dist.Normal(beta_mu, beta_sigma).to_event(1))
+
+
+
+    with pyro.plate("data", X.shape[0]):
+        l = pyro.sample("l", dist.Bernoulli(l_prior).to_event(1), obs=categoricals)
+        beta_l = beta.matmul(l)
+        logits = alpha + X.matmul(beta_l)
+        # If you use logits you don't need to do sigmoid
+        y = pyro.sample("y",
+                        dist.Categorical(logits=logits), obs=obs)
+
+    return y
